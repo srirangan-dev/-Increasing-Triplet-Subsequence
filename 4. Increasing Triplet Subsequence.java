89985ad53672ce740b67e70a98add6ce79dc2162
Sri Rangan <srirangan0603@gmail.com>
2026-03-01 11:09:37 +0530

    Create 4. Increasing Triplet Subsequence.java

---
+++ 4. Increasing Triplet Subsequence.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean increasingTriplet(int[] nums) {
+        int first=Integer.MAX_VALUE;
+        int second=Integer.MAX_VALUE;
+
+        for(int i:nums){
+
+            if(i<=first){
+                first=i;
+            }
+        else if(i<=second){
+            second=i;
+        }
+
+        else{
+            return true;
+        }
+
+
+
+        }
+
+        return false;
+
+        
+    }
+}
